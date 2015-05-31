6e25bab7960dffbb5b1302dd9ca61a5eaa46d411
Raymond Sapida <ray.sapida@gmail.com>
2015-05-31 08:09:37 -0700

    Add the titles to the search results div

--- apps/app.js
+++ apps/app.js
@@ -5,7 +5,10 @@ $(function(){
 });
 
 function showResults(results){
+  var html = "";
   $.each(results, function(index,value){
+    html += '<p>' + value.Title + '</p>';
     console.log(value.Title);
   });
+  $('#search-results').html(html);
 }
