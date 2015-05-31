2a5f12b1fe2551d985bec99a6eae2082da8e09fa
Raymond Sapida <ray.sapida@gmail.com>
2015-05-31 08:15:49 -0700

    Connect search input with the api

--- apps/app.js
+++ apps/app.js
@@ -1,7 +1,12 @@
 $(function(){
-  $.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
-    showResults(data.Search);
-  })
+  $('#search-term').submit(function(event){
+    event.preventDefault();
+    var searchTerm = $('#query').val();
+    alert(searchTerm);
+    $.getJSON('http://www.omdbapi.com/?s=' + searchTerm + '&r=json', function(data){
+      showResults(data.Search);
+    });
+  });
 });
 
 function showResults(results){
