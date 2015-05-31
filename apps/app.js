9c16c0ac1045dce21088630c700a2b885fac3c3e
Raymond Sapida <ray.sapida@gmail.com>
2015-05-31 08:07:55 -0700

    Extract console logs to its own function

--- apps/app.js
+++ apps/app.js
@@ -1,8 +1,11 @@
 $(function(){
   $.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
-    myData = data.Search;
-    $.each(myData, function(index,value){
-      console.log(value.Title);
-    });
+    showResults(data.Search);
   })
 });
+
+function showResults(results){
+  $.each(results, function(index,value){
+    console.log(value.Title);
+  });
+}
